cb4d768244b090662f57c250bfaf5a4acc73f218
chendongdong <cdd@simplexai.com>
2026-02-15 13:41:10 +0800

    Set v0.2.0 checksum for oldwinter-greet formula and cask

--- Casks/oldwinter-greet.rb
+++ Casks/oldwinter-greet.rb
@@ -1,6 +1,6 @@
 cask "oldwinter-greet" do
   version "0.2.0"
-  sha256 "REPLACE_ME"
+  sha256 "6e7c047c7fbc670bf3d9dd9f02a03d75642cb0fe92ad3b2d68b8e655d03e644a"
 
   url "https://github.com/oldwinter/homebrew/archive/refs/tags/v#{version}.tar.gz"
   name "oldwinter-greet"
--- Formula/oldwinter-greet.rb
+++ Formula/oldwinter-greet.rb
@@ -2,7 +2,7 @@ class OldwinterGreet < Formula
   desc "Simple greeting CLI for oldwinter"
   homepage "https://github.com/oldwinter/homebrew"
   url "https://github.com/oldwinter/homebrew/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "REPLACE_ME"
+  sha256 "6e7c047c7fbc670bf3d9dd9f02a03d75642cb0fe92ad3b2d68b8e655d03e644a"
   head "https://github.com/oldwinter/homebrew.git", branch: "main"
   license "MIT"
 
